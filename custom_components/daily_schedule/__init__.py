dcea3f8092b1eff2df36ab48c1ddbddd72c76a50
amittein <amittein@gmail.com>
2024-06-14 07:08:32 +0000

    async_reload -> async_schedule_reload

--- custom_components/daily_schedule/__init__.py
+++ custom_components/daily_schedule/__init__.py
@@ -17,7 +17,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def config_entry_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Update listener, called when the config entry options are changed."""
-    await hass.config_entries.async_reload(entry.entry_id)
+    hass.config_entries.async_schedule_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
